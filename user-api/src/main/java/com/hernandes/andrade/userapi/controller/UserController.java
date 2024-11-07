eac6a111eb32ec57b815ab599f6f84bda14ffd01
Hernandes <cfhernandesandrade@gmail.com>
2024-11-07 12:46:21 -0300

    adjusting the user controller to change methods now using the user service and not using the memory list data - user-api.

--- user-api/src/main/java/com/hernandes/andrade/userapi/controller/UserController.java
+++ user-api/src/main/java/com/hernandes/andrade/userapi/controller/UserController.java
@@ -1,83 +1,55 @@
 package com.hernandes.andrade.userapi.controller;
 
 import com.hernandes.andrade.userapi.dto.UserDTO;
+import com.hernandes.andrade.userapi.service.UserService;
 import jakarta.annotation.PostConstruct;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
 
-    @GetMapping("/")
-    public String getMessage() {
-        return "Spring Boot is working!";
-    }
-
-    public static List<UserDTO> users = new ArrayList<>();
-
-    @PostConstruct
-    public void initiateList() {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setName("Pedro");
-        userDTO.setCpf("12345678911");
-        userDTO.setAddress("Travessa Fulano 01");
-        userDTO.setEmail("pedro@teste.com");
-        userDTO.setPhone("22999887766");
-        userDTO.setCreatedAt(LocalDateTime.now());
-
-        UserDTO userDTO2 = new UserDTO();
-        userDTO2.setName("Tiago");
-        userDTO2.setCpf("12345678912");
-        userDTO2.setAddress("Travessa Fulano 02");
-        userDTO2.setEmail("tiago@teste.com");
-        userDTO2.setPhone("21999887766");
-        userDTO2.setCreatedAt(LocalDateTime.now());
-
-        UserDTO userDTO3 = new UserDTO();
-        userDTO3.setName("João");
-        userDTO3.setCpf("12345678913");
-        userDTO3.setAddress("Avenida A");
-        userDTO3.setEmail("joao@teste.com");
-        userDTO3.setPhone("22988776655");
-        userDTO3.setCreatedAt(LocalDateTime.now());
-
-        users.add(userDTO);
-        users.add(userDTO2);
-        users.add(userDTO3);
-    }
+    private final UserService userService;
 
     @GetMapping("/users")
     public List<UserDTO> getUsers() {
-        return users;
+        return userService.getAllUsers();
     }
 
-    @GetMapping("/{cpf}")
-    public UserDTO getFilterUser(@PathVariable String cpf) {
-        return users
-                .stream()
-                .filter(userDTO -> userDTO.getCpf().equals(cpf))
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("User not found"));
+    @GetMapping("/{id}")
+    public UserDTO findById(@PathVariable("id") Integer id) {
+        return userService.findById(id);
     }
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
     public UserDTO registerUser(@RequestBody @Valid UserDTO userDTO) {
-        userDTO.setCreatedAt(LocalDateTime.now());
-        users.add(userDTO);
-        return userDTO;
+        return userService.registerUser(userDTO);
     }
 
-    @DeleteMapping("/delete/{cpf}")
-    public boolean deleteUser(@PathVariable String cpf) {
-        return users
-                .removeIf(userDTO -> userDTO.getCpf().equals(cpf));
+    @GetMapping("/cpf/{cpf}")
+    public UserDTO findByCpf(@PathVariable("cpf") String cpf) {
+        return userService.findByCpf(cpf);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable("id") Integer id) throws UserPrincipalNotFoundException {
+        userService.deleteUser(id);
+    }
+
+    @GetMapping("/search")
+    public List<UserDTO> queryByName(@RequestParam(name = "nome", required = true) String name) {
+        return userService.findByQueryByName(name);
     }
 
 }
