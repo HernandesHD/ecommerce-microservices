cefafac0509308bab62cf24410c5efe10cc83cec
Hernandes <cfhernandesandrade@gmail.com>
2024-11-08 00:50:22 -0300

    first commit - product-api.

---
+++ product-api/src/test/java/com/hernandes/andrade/study/productapi/ProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hernandes.andrade.study.productapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
