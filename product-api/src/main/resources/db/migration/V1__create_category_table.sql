def0dc178210d53662127954490305aa1a842807
Hernandes <cfhernandesandrade@gmail.com>
2024-11-09 23:50:11 -0300

    creating predefined data insertion and renaming column - product-api.

--- product-api/src/main/resources/db/migration/V1__create_category_table.sql
+++ product-api/src/main/resources/db/migration/V1__create_category_table.sql
@@ -2,5 +2,5 @@ CREATE SCHEMA IF NOT EXISTS products;
 
 CREATE TABLE products.category {
     id bigserial primary key,
-    nome_category varchar(100) not null
+    name_category varchar(100) not null
 }
---
+++ product-api/src/main/resources/db/migration/V3__insert_categories.sql
@@ -0,0 +1,3 @@
+INSERT INTO products.category(id, name_category) VALUES (1, 'Eletronico');
+INSERT INTO products.category(id, name_category) VALUES (2, 'Móveis');
+INSERT INTO products.category(id, name_category) VALUES (3, 'Brinquedo');
