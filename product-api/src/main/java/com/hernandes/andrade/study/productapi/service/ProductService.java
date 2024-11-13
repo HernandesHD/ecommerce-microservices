4461923fab6315b59acce316a958d0aa50c034f4
Hernandes <cfhernandesandrade@gmail.com>
2024-11-12 23:55:13 -0300

    Implementing delete method in ProductService class - product-api.

--- product-api/src/main/java/com/hernandes/andrade/study/productapi/service/ProductService.java
+++ product-api/src/main/java/com/hernandes/andrade/study/productapi/service/ProductService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -42,6 +43,13 @@ public class ProductService {
     public ProductDTO save(ProductDTO productDTO) {
         Product product = productRepository.save(Product.convert(productDTO));
         return ProductDTO.convert(product);
+    }
+
+    public void delete(Long productId) {
+        Optional<Product> productOptional = productRepository.findById(productId);
+        if (productOptional.isPresent()) {
+            productRepository.delete(productOptional.get());
+        }
     }
 
 }
